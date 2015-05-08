e2f3cc75ca6e4e93faf6ff49b0c0da394b60d617
Gunther Groenewege <gunther@groenewege.com>
2015-05-08 20:58:53 +0200

    spanish language file

---
+++ es.php
@@ -0,0 +1,13 @@
+<?php
+
+l::set('your_username', 'Nombre de Usuario');
+l::set('your_password', 'Contraseña');
+l::set('log_in', 'Identificarse');
+l::set('log_in_error', 'Identificación incorrecta');
+l::set('log_out', 'Desconectarse');
+l::set('contact_us', 'Contacto');
+l::set('view', 'Consultar');
+l::set('download', 'Descargar');
+l::set('back', 'Volver');
+l::set('projects', 'Proyectos');
+l::set('name', 'Nombre');
